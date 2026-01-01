854bb7ec29dc0c8fdaa0cb65d100f5752de07cfd
keltulsan <ewen.beaufils09@gmail.com>
2026-01-01 20:04:21 +0100

    feature : moreinfo page

--- Front/my-app/src/JS/MoreInfo.js
+++ Front/my-app/src/JS/MoreInfo.js
@@ -1,11 +1,43 @@
 import {Background} from './Backgound'
+import { useParams } from 'react-router-dom';
+import projects from '../JSON/projects.json'
+import { useNavigate } from 'react-router-dom';
+import '../Css/MoreInfo.css'
 
-export function ProjectMoreInfo(id) {
+export function ProjectMoreInfo() {
+    const navigate = useNavigate();
+
+    const { id } = useParams();
+    var projectName = "";
+    var projectDescription = "";
+    
+    for (const project of projects) {
+        if(project.id == id) {
+            projectName = project.name;
+            projectDescription = project.description;
+            break;
+        }
+    }
+
+    const OnClickHome = () => {
+        navigate('/');
+    }
+
     return (
         <div>
             <Background />
 
-            <h1>Project</h1>
+            <div className='container'>
+
+                <h1 className='title'>{projectName}</h1>
+
+                <div className='description-container'>
+                    <p className='description'>{projectDescription}</p>
+                </div>
+
+                <button className='home-button' onClick={OnClickHome}>Home</button>
+            
+            </div>
         </div>
     )
 }
